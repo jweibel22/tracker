00e8ba753badea24f9f300220082b75f23ea427c
Jimmy Rasmussen <jweibel_22@hotmail.com>
2026-01-31 16:57:04 +0100

    Add github workflow

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 import { VitePWA } from 'vite-plugin-pwa'
 
 export default defineConfig({
+  base: '/tracker/', // Change to '/' if using username.github.io root
   plugins: [
     react(),
     VitePWA({
